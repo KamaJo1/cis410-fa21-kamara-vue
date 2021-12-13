05f10b4f144a6794251e5c2aacff6a67c0973854
John Doe <josephka@colostate.edu>
2021-12-12 17:40:32 -0700

    14.2 12/2 header, logout, /account guard

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import {createRouter, createWebHistory} from 'vue-router';
 
+
 import Home from "./components/Home.vue";
 import Login from "./components/Login.vue";
 import WorkDetail from "./components/WorkDetail.vue";
@@ -8,11 +9,16 @@ import NotFound from "./components/NotFound.vue";
 import ReviewCreate from "./components/ReviewCreate.vue";
 import Signup from "./components/Signup.vue";
 import Account from "./components/Account.vue";
+import myStore from "./store.js";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [{path: "/", component: Home},
-            {path: '/account', component: Account},
+            {path: '/account', component: Account, beforeEnter(to,from,next){
+                 if(myStore.state.token){next();}else{
+                      next("/login");
+                 }
+            }},
             {path: "/login", component: Login},
             {path: "/Work", component: Work},
             {path: "/Work/:pk",component: WorkDetail,
--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import { createStore } from "vuex";
 import axios from "axios";
+import myRoutes from "./routes.js";
 export default createStore({
   State: {
     token: null,
@@ -15,14 +16,28 @@ export default createStore({
     },
     storeWork(state, Work){
         state.work = Work;
-    }
+    },
+    clearAuthData(state){
+      state.token = null;
+      state.user = null;
+    },
+
   },
   actions: {
-    getWork(commit) {
-      axios.get("/Work").then((aResponse) => {
+    getWork({ commit }) {
+      axios.get("/work").then((aResponse) => {
         console.log("response in Work", aResponse);
         commit("storeWork", aResponse.data);
       });
     },
+    logout({ commit,state }){
+axios.post("/Customer/logout",null,{
+  headers:{Authorization: `Bearer ${state.token}`
+  },
+}).then(()=>{
+  commit("clearAuthData");
+myRoutes.replace("/");
+}).catch(()=>{ console.log("error in loggin out")})
+    }
   },
 });
